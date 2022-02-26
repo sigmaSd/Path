967255cec9a0eac705433f864474ea74afefc1b8
Nbiba Bedis <bedisnbiba@gmail.com>
2022-02-26 10:37:49 +0100

    upgrade private

--- path.ts
+++ path.ts
@@ -10,15 +10,15 @@ String.prototype.asPath = function (): Path {
 };
 
 export class Path {
-  private path: string;
+  #path: string;
   constructor(path: string) {
-    this.path = path;
+    this.#path = path;
   }
   toString(): string {
-    return this.path;
+    return this.#path;
   }
   isAbsoulte(): boolean {
-    return path.isAbsolute(this.path);
+    return path.isAbsolute(this.#path);
   }
   isRelative(): boolean {
     return !this.isAbsoulte();
@@ -27,7 +27,7 @@ export class Path {
     return this.parent() !== undefined;
   }
   parent(): Path | undefined {
-    const parsedPath = path.parse(this.path);
+    const parsedPath = path.parse(this.#path);
     if (parsedPath.base === "") {
       return undefined;
     }
@@ -37,32 +37,32 @@ export class Path {
     return new Ancestors(this);
   }
   fileName(): string | undefined {
-    const base = path.parse(this.path).base;
+    const base = path.parse(this.#path).base;
     if (base === "" || base === "..") {
       // Rust does this
       return undefined;
     }
-    return path.basename(path.resolve(this.path));
+    return path.basename(path.resolve(this.#path));
   }
   isDir(): boolean {
     try {
-      return Deno.statSync(this.path).isDirectory;
+      return Deno.statSync(this.#path).isDirectory;
     } catch {
       return false;
     }
   }
   isFile(): boolean {
     try {
-      return Deno.statSync(this.path).isFile;
+      return Deno.statSync(this.#path).isFile;
     } catch {
       return false;
     }
   }
   join(otherPath: Path | string): Path {
     if (typeof otherPath === "string") {
-      return new Path(customJoin(this.path, otherPath));
+      return new Path(customJoin(this.#path, otherPath));
     } else {
-      return new Path(customJoin(this.path, otherPath.path));
+      return new Path(customJoin(this.#path, otherPath.#path));
     }
   }
   iter(): Iterator<string> {
@@ -99,8 +99,8 @@ export class Path {
     );
   }
   equals(otherPath: Path): boolean {
-    const me = path.parse(this.path);
-    const other = path.parse(otherPath.path);
+    const me = path.parse(this.#path);
+    const other = path.parse(otherPath.#path);
     return me.root === other.root && me.dir === other.dir &&
       me.base === other.base && me.ext === other.ext && me.name === other.name;
   }
@@ -144,19 +144,19 @@ export class Path {
   }
   metaData(): Deno.FileInfo | undefined {
     try {
-      return Deno.statSync(this.path);
+      return Deno.statSync(this.#path);
     } catch {
       undefined;
     }
   }
   canonicalize(): Path {
-    return new Path(path.resolve(this.path));
+    return new Path(path.resolve(this.#path));
   }
   readLink(): Path {
-    return new Path(Deno.readLinkSync(this.path));
+    return new Path(Deno.readLinkSync(this.#path));
   }
   readDir(): Iterable<Deno.DirEntry> {
-    return Deno.readDirSync(this.path);
+    return Deno.readDirSync(this.#path);
   }
   exists(): boolean {
     if (this.metaData()) {
@@ -165,7 +165,7 @@ export class Path {
     return false;
   }
   isSymlink(): boolean {
-    return Deno.lstatSync(this.path).isSymlink;
+    return Deno.lstatSync(this.#path).isSymlink;
   }
   withFileName(name: string): Path {
     return this.parent() ? this.parent()!.join(name) : name.asPath();
@@ -181,14 +181,14 @@ export class Path {
 }
 
 export class Ancestors implements Iterator<Path> {
-  private startPath: Path | undefined;
+  #startPath: Path | undefined;
   constructor(startPath: Path) {
-    this.startPath = startPath;
+    this.#startPath = startPath;
   }
   next(): IteratorResult<Path, undefined> {
-    const nextPath = this.startPath;
-    if (this.startPath) {
-      this.startPath = this.startPath.parent();
+    const nextPath = this.#startPath;
+    if (this.#startPath) {
+      this.#startPath = this.#startPath.parent();
       return { done: false, value: nextPath! }; //nextPath is defined
     } else {
       return { done: true, value: undefined };
